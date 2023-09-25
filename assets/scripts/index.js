ad29f4531b123e7c7f6e63b6fdd914e136f0bb07
mthiagoalves <m.thiago.alves@live.com>
2023-09-25 15:13:30 +0100

    feat(section)

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,29 +1,69 @@
 
-const picturesContainerLeft = document.getElementsByClassName('picture-curtain')[0].getClientRects()[0].left;
-let foregroundPicture = document.getElementsByClassName('foreground-picture-container')[0];
+const imageAfter = document.querySelector('.compare__after');
+const widthImageAfter = imageAfter.offsetWidth;
+const heightImageAfter = imageAfter.offsetHeight;
+const delimeter = document.querySelector('.compare__delimeter')
+let isActive = false;
 
-function curtainEffect(foregroundPictureWidth) {
+compare();
+
+function compare(){
+  delimeter.style.left = (widthImageAfter/2) - (delimeter.offsetWidth/2) + 'px'; 
+  imageAfter.style.width = '50%';
   
-  // do following for Method-1 only to use percentage
-  // foregroundPictureWidth = foregroundPictureWidth / 5;
-  // foregroundPicture.style.width = foregroundPictureWidth + '%';
-  
-  // do following for both Method-1 and Method-2
-  foregroundPicture.style.width = foregroundPictureWidth + 'px';
-  
+  setListeners()
 }
 
-function curtainEffectHandler(e) {
-  window.requestAnimationFrame((timestamp) => {
-    // Method - 1
-    // ----------
-    curtainEffect(e.pageX - picturesContainerLeft);
-    
-    
-    // Method - 2
-    // ----------
-    // this simplest way is to use the x offset from container (element responsible for the event)
-    // uncooment the pixel value scenario in curtainEffect()
-    //curtainEffect(e.offsetX);
-  });
+function setListeners(){
+  delimeter.addEventListener('mousedown', activeDelimeter);
+  delimeter.addEventListener('touchstart', activeDelimeter);
+  
+  window.addEventListener('mousemove', onMoveDelimeter);
+  window.addEventListener('touchmove', onMoveDelimeter);
+  
+  
+  window.addEventListener('mouseup', () => {
+    isActive = false;
+  })
+   window.addEventListener('touchstop', () => {
+    isActive = false;
+  })
 }
+
+
+function activeDelimeter(event) {
+  event.preventDefault();
+  
+  isActive = true;
+}
+
+function onMoveDelimeter(event){
+  if(!isActive) return
+  
+  moveSlide(currentPositionDelimeter(event))
+}
+
+function currentPositionDelimeter(event) {
+  let image = imageAfter.getBoundingClientRect();
+  let x = 0;
+  x = event.pageX - image.left;
+  
+  if (x < 0) {
+    x = 0;
+  }
+  
+  if ( x > widthImageAfter) {
+    x = widthImageAfter
+  }
+  
+  return x
+}
+
+
+function moveSlide (x){
+  imageAfter.style.width = x + 'px';
+  
+  delimeter.style.left = imageAfter.offsetWidth - (delimeter.offsetWidth / 2 ) + 'px';
+
+}
+
